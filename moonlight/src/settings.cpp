2bf70fa3bf2a35fd895f30655b4d649d7aefee9c
AorsiniYT <aorsinigamer@gmail.com>
2025-07-09 20:58:39 -0600

    refactor: update include paths in check_host and settings

--- moonlight/src/settings.cpp
+++ moonlight/src/settings.cpp
@@ -13,7 +13,7 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 */
-#include "Settings.hpp"
+#include "settings.hpp"
 #include "ConfigManager.hpp"
 #include "tab/settings_tab.hpp"
 #include <string>
