10081af378dfca60fb3b4cb6bcebb4a2446ce7c4
AorsiniYT <aorsinigamer@gmail.com>
2025-11-19 19:02:51 -0600

    fix: Cleanup

--- moonlight/include/debug.hpp
+++ moonlight/include/debug.hpp
@@ -8,3 +8,12 @@ __attribute__((format(printf, 1, 2))) void vita_debug_log(const char* fmt, ...);
 #ifdef __cplusplus
 }
 #endif
+
+// Size printing compatibility for platforms without "%zu" support (PSVita)
+#if defined(__PSP2__) || defined(VITA) || defined(__VITA__) || defined(PSVITA) || defined(__PSVITA__) || defined(__vita__)
+#define FMT_SIZE_T "%lu"
+#define FMT_SIZE_CAST(x) ((unsigned long)(x))
+#else
+#define FMT_SIZE_T "%zu"
+#define FMT_SIZE_CAST(x) (x)
+#endif
